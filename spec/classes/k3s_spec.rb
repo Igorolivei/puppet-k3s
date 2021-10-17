8f9f97733ef8714b5fee436323061e53dabad22e
Philip Freude <philipfreude@gmail.com>
2021-10-17 11:47:30 +0200

    Fix unit tests
    - remove tests for install and uninstall classes as they are private classes and should not be instantiated directly
    - extend test for k3s class to test present and absent cases

--- spec/classes/k3s_spec.rb
+++ spec/classes/k3s_spec.rb
@@ -4,10 +4,27 @@ require 'spec_helper'
 
 describe 'k3s' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
+    context "install - on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          ensure: 'present',
+        }
+      end
 
       it { is_expected.to compile }
+      it { is_expected.to contain_class('k3s::install') }
+    end
+    context "uninstall - on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          ensure: 'absent',
+        }
+      end
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('k3s::uninstall') }
     end
   end
 end
